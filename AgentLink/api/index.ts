b65c6e0fa84c36713e9cb7d719fcd805f1026c77
Sambit Sargam Ekalabya <sambitsargam2003@gmail.com>
2025-02-07 03:37:32 +0530

    finished deployment

--- AgentLink/api/index.ts
+++ AgentLink/api/index.ts
@@ -45,21 +45,21 @@ const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_A
             modespray(),
             coingecko({ apiKey: "CG-omKTqVxpPKToZaXWYBb8bCJJ" }),
             opensea(process.env.OPENSEA_API_KEY as string),
-           // pumpfun(),
-           modeGovernance(),
+            // pumpfun(),
+            modeGovernance(),
         ],
     });
 
     const app = express();
-// Parse URL-encoded bodies (as sent by HTML forms)
-app.use(bodyParser.urlencoded({ extended: true }));
+    // Parse URL-encoded bodies (as sent by HTML forms)
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-// Parse JSON bodies (as sent by API clients)
-app.use(bodyParser.json());
+    // Parse JSON bodies (as sent by API clients)
+    app.use(bodyParser.json());
 
     app.post("/api/send-whatsapp", async (req, res) => {
         console.log("Headers:", req.headers);
-        console.log("Body:", req.body);  
+        console.log("Body:", req.body);
         const from = req.body.From;  // The sender's phone number
         const body = req.body.Body;
         console.log("Received WhatsApp message from", from, "with body:", body);
@@ -72,6 +72,8 @@ app.use(bodyParser.json());
                 prompt: body,
             });
 
+            console.log("AI response:", result.text);
+
             const message = await twilioClient.messages.create({
                 to: `${from}`,
                 from: `whatsapp:${process.env.TWILIO_WHATSAPP_NUMBER}`,
@@ -79,6 +81,12 @@ app.use(bodyParser.json());
             });
             res.json({ success: true, message: "WhatsApp message sent with AI response.", sid: message.sid });
         } catch (error) {
+            const message = await twilioClient.messages.create({
+                to: `${from}`,
+                from: `whatsapp:${process.env.TWILIO_WHATSAPP_NUMBER}`,
+                body: "Sorry, Currenlty I am not able to process your request. Please try again later."
+            });
+
             console.error("Failed to send WhatsApp message with AI response:", error);
             res.status(500).json({ success: false, message: "Failed to send WhatsApp message." });
         }
@@ -108,7 +116,7 @@ app.use(bodyParser.json());
         }
     });
 
-    const PORT = process.env.PORT || 8080;
+    const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
